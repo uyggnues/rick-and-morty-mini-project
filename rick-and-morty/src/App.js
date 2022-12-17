457ca0591d74a7878e64970bf183cc757e46cc8b
Seunggyu <seunggyul14@icloud.com>
2022-12-16 16:06:28 -0800

    info

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -1,14 +1,28 @@
 // import './App.css';
 // import React from "react"
 import CharacterList from "./components/CharacterList"
-import characters from "./data/characters"
+// import characters from "./data/characters"
 import SearchForm from "./components/SearchForm"
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 // import Character from "./components/Character
 function App() {
 
   const [queryChar, setQueryChar] = useState("")
+  const [characters, setCharacters] = useState([])
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+      const resp = await fetch(`http://localhost:3000/characters/`)
+      const characterList = await resp.json()
+      setCharacters(characterList)
+    } catch (error) {
+      alert(error)
+    }
+  }
+    fetchData()
+  }, [])
+  
   const filteredChar = characters.filter(character => character.name.toLowerCase().includes(queryChar.toLowerCase()))
   return (
     <div className="App">
@@ -19,3 +33,23 @@ function App() {
 }
 
 export default App;
+
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     try {
+    //       const resp = await fetch(`http://localhost:3000/characters`, {
+    //         method: "POST",
+    //         headers: {
+    //           'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({name: "Matteo", species: "Weird alien teacher", })
+    //       })
+    //       const newCharacter = await resp.json()
+    //       setCharacters(currentCharacters => [...currentCharacters, newCharacter])
+    //     } catch (error) {
+    //       alert(error)
+    //     }
+    //   }
+  
+    //   fetchData()
+    // }, [])
--- rick-and-morty/src/components/Character.js
+++ rick-and-morty/src/components/Character.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import {useState} from 'react'
 
  
  const Character = ({id, url, image, name, status, species, type, gender}) => {
 
+   const [showImage, setShowImage] = useState(true);
+   
+   const handleMouseAction = () => {
+      setShowImage(currentValue => !currentValue)
+   }
+
+
+
     return (
-        <div className="Character" id={id}>
+        <div className="Character" id={id} onMouseEnter={handleMouseAction} onMouseLeave={handleMouseAction} >
              <li>
                  <a href={url} target="_blank" rel="noreferrer"><h4>{name}</h4></a>
-                 <img src={image} alt={name} />
+                 {
+                 showImage ? <img src={image} alt={name} /> :
+                 <>
                  <span className="card-detail">Status: {status}</span>
                  <span className="card-detail">Species: {species}</span>
                  <span className="card-detail">Type: {type}</span>
                  <span className="card-detail">Gender: {gender}</span>
+                 </>
+                 }
               </li>
          </div>
     )
